e13ea9c3575232998351a73da631fb2d5fbf4be9
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2019-05-25 20:33:18 -0700

    Fixing bash function placement

--- bin/tf_bootstrap.sh
+++ bin/tf_bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+validate_project() { echo "$PROJECT_LIST" | grep -F -q -x "$1"; }
+
+sed_tfvars() {
+    sed -i "s/__PROJECT_NAME__/$1/" ../terraform/terraform.tfvars
+}
+
 clear
 
 if [ -z $GOOGLE_CLOUD_PROJECT ]
@@ -17,9 +23,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
   sed_tfvars $GOOGLE_CLOUD_PROJECT
 else
-  read -p "Would you like to specify a project ID here (alternatively you can switch projects in cloud shell and rerun this script)? (y/n)" -n 1 -r
+  read -p "Would you like to specify a project ID here (alternatively you can switch projects in cloud shell and rerun this script)? (y/n) " -n 1 -r
   if [[ $REPLY =~ ^[Yy]$ ]]
-    echo "Gathering available projects . . ."
+  then
+    echo -e "\nGathering available projects . . ."
     PROJECT_LIST=$(gcloud projects list | awk '{ print $1 }' | tail -n +2)
     echo "The following projects are available to your cloud shell"
     for i in $PROJECT_LIST; do
@@ -28,14 +35,8 @@ else
     read -p "What is the ID of the GCP project you'd like to use?" -r project_id
     validate_project "$project_id" \
     && sed_tfvars "$project_id" \
-    || $(echo "That project isn't in available in this GCP account.";\
+    || (echo "That project isn't in available in this GCP account.";\
          echo "Start a new project through the GCP console, or figure out what project you want to use and try again.";\
          echo "You can return to this script by running $(pwd)/capstone/bin/tf_bootstrap.sh"; exit 1)
   fi
 fi
-
-validate_project() { echo "$PROJECT_LIST" | grep -F -q -x "$1"; }
-
-sed_tfvars() {
-    sed -i "s/__PROJECT_NAME__/$1/" ../terraform/terraform.tfvars
-}
