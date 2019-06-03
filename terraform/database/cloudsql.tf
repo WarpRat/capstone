c125266165f4fadde316934908dc5ac85fe92053
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2019-06-02 23:47:39 -0700

    Adding logic to avoid sql name collisions

--- terraform/database/cloudsql.tf
+++ terraform/database/cloudsql.tf
@@ -1,6 +1,13 @@
+# Generating random ID to avoid this:
+# Error 409: The Cloud SQL instance already exists., instanceAlreadyExists.
+# This may be due to a name collision - SQL instance names cannot be reused within a week.
+resource "random_id" "db_name" {
+  byte_length = 1
+}
+
 resource "google_sql_database_instance" "gitlabsql" {
   provider         = "google-beta"
-  name             = "gitlab-cloudsql"
+  name             = "gitlab-cloudsql-${random_id.db_name}"
   region           = "${var.region}"
   database_version = "POSTGRES_9_6"
 
@@ -18,6 +25,13 @@ resource "google_sql_database_instance" "gitlabsql" {
       private_network = "${data.google_compute_network.default.self_link}"
     }
   }
+}
+
+# Write the db name for use later
+
+resource "google_compute_project_metadata_item" "gitlab_db_name" {
+  key   = "db_name"
+  value = "${google_sql_database_instance.gitlabsql.name}"
 }
 
 resource "google_sql_user" "gitlab_user" {
