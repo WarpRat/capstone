27d25e52410da819afb70d5e541c2dbdcaa4d05f
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2019-06-02 23:27:10 -0700

    typo

--- terraform/service-accounts/gitlab_sa.tf
+++ terraform/service-accounts/gitlab_sa.tf
@@ -12,7 +12,7 @@ module "gitlab_sa" {
 
 resource "null_resource" "write_key" {
   provisioner "local-exec" {
-    command = "cd $HOME/.capstone_secure && gcloud ima service-accounts keys screate --iam-account ${module.gitlab_sa.email} gcs-key.json"
+    command = "cd $HOME/.capstone_secure && gcloud iam service-accounts keys screate --iam-account ${module.gitlab_sa.email} gcs-key.json"
   }
 
   triggers {
