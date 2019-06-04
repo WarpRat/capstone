b73cf63e2d1175ab62ecdb3f1652f3e9a032d239
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2019-06-03 23:01:15 -0700

    Missed values reference

--- bin/k8s_bootstrap.sh
+++ bin/k8s_bootstrap.sh
@@ -16,9 +16,9 @@ helm version > /dev/null 2>&1 && echo "Helm appears to have initialized inside y
                               || echo "Something appears to have gone wrong with helm. Please try again."
 
 helm upgrade capstone ./capstone/helm/charts/common --install
-helm upgrade gitlab-pg ./capstone/helm/charts/secrets --install --set seclit.password=$(cat .capstone_secure/db.pw)
-helm upgrade google-application-credentials ./capstone/helm/charts/secrets --install --set seclit.gcs-application-credentials-file=$(cat .capstone_secure/gcs-key.json)
-PROJECT_ID=$(gcloud config get-value project) helm upgrade gitlab-rails-storage ./capstone/helm/charts/secrets --install \
+helm upgrade gitlab-pg ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/gitlab-pg.yaml --install --set seclit.password=$(cat .capstone_secure/db.pw)
+helm upgrade google-application-credentials ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/storage-creds.yaml --install --set seclit.gcs-application-credentials-file=$(cat .capstone_secure/gcs-key.json)
+PROJECT_ID=$(gcloud config get-value project) helm upgrade gitlab-rails-storage ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/rails.yaml --install \
                                                     --set seclit.connection.google_project=${PROJECT_ID} \
                                                     --set seclit.connection.google_client_email=gitlab-storage-sa@${PROJECT_ID}.iam.gserviceaccount.com \
                                                     --set seclit.connection.google_json_key_string=$(cat .capstone_secure/gcs-key.json)
