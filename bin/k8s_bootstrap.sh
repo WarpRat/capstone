e41545765b7262de9733cf70cda81732a112049f
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2019-06-12 20:00:13 -0700

    Rework secrets to handle multiline json

--- bin/k8s_bootstrap.sh
+++ bin/k8s_bootstrap.sh
@@ -22,10 +22,12 @@ echo "Checking that helm is properly initialized"
 helm version > /dev/null 2>&1 && echo "Helm appears to have initialized inside your cluster properly." \
                               || echo "Something appears to have gone wrong with helm. Please try again."
 
-helm upgrade capstone ./capstone/helm/charts/common --install
+cp .capstone_secure/gcs-key.json ./capstone/helm/charts/secrets
+helm upgrade gitlab-ns ./capstone/helm/charts/common --install
 helm upgrade gitlab-pg ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/gitlab-pg.yaml --install --set seclit.password=$(cat .capstone_secure/db.pw)
-helm upgrade google-application-credentials ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/storage-creds.yaml --install --set seclit.gcs-application-credentials-file=$(cat .capstone_secure/gcs-key.json)
+helm upgrade google-application-credentials ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/storage-creds.yaml --install --set fileLit.gcs-application-credentials-file=gcs-key.json
 PROJECT_ID=$(gcloud config get-value project) helm upgrade gitlab-rails-storage ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/rails.yaml --install \
                                                     --set seclit.connection.google_project=${PROJECT_ID} \
                                                     --set seclit.connection.google_client_email=gitlab-storage-sa@${PROJECT_ID}.iam.gserviceaccount.com \
-                                                    --set seclit.connection.google_json_key_string=$(cat .capstone_secure/gcs-key.json)
+                                                    --set fileLit.connection.google_json_key_string=gcs-key.json
+rm ./capstone/helm/charts/secrets/gcs-key.json
