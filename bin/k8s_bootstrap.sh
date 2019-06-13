eff6a81bb7eb6818767f38a90f81c66215f54ec1
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2019-06-13 01:03:34 -0700

    fixing paths after local troubleshooting

--- bin/k8s_bootstrap.sh
+++ bin/k8s_bootstrap.sh
@@ -21,14 +21,14 @@ helm version > /dev/null 2>&1 && echo "Helm appears to have initialized inside y
                               || echo "Something appears to have gone wrong with helm. Please try again."
 
 cp ~/.capstone_secure/gcs-key.json ~/capstone/helm/charts/secrets/
-helm upgrade gitlab-ns ./capstone/helm/charts/common --install
-helm upgrade gitlab-pg ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/gitlab-pg.yaml --install --set seclit.password=$(cat .capstone_secure/db.pw) --namespace gitlab
+helm upgrade gitlab-ns ~/capstone/helm/charts/common --install
+helm upgrade gitlab-pg ~/capstone/helm/charts/secrets -f ~/capstone/helm/charts/secrets/values/gitlab-pg.yaml --install --set seclit.password=$(cat .capstone_secure/db.pw) --namespace gitlab
 helm upgrade google-application-credentials ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/storage-creds.yaml --install --set fileLit.gcs-application-credentials-file=gcs-key.json --namespace gitlab
-helm upgrade gitlab-rails-storage ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/rails.yaml --install --namespace gitlab \
+helm upgrade gitlab-rails-storage ~/capstone/helm/charts/secrets -f ~/capstone/helm/charts/secrets/values/rails.yaml --install --namespace gitlab \
                                                     --set seclit.google_project=${GOOGLE_CLOUD_PROJECT} \
                                                     --set seclit.google_client_email=gitlab-storage-sa@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com \
                                                     --set fileLit.google_json_key_string=gcs-key.json \
                                                     --set extrakey=connection
-rm ./capstone/helm/charts/secrets/gcs-key.json
+rm ~/capstone/helm/charts/secrets/gcs-key.json
 
 # Set environment variables for everything we need to sed into the gitlab values.yaml
