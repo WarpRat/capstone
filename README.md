6bb4492589d8921832ce23d632ce86084c6b6e63
WarpRat <23439640+WarpRat@users.noreply.github.com>
2019-06-13 22:03:03 -0700

    Everything is working. Pull in all dev commits.
    * Adding GKE TF module and first pass at module invocation
    
    * Restructure terraform for multiple steps
    
    * Changing terraform structure, adding service account module
    
    * Add an easy way to avoid merging to master before tests
    
    * Add some more console logging
    
    * Pull out unused function code
    
    * Adding storage and database terraform files
    
    * Pass password via cli
    
    * Several fixes
    
    * Several fixes
    
    * turning on beta provider where needed
    
    * Fixing typos
    
    * Adding a cleanup script and changing service account secret handling
    
    * Making cleanup script executable
    
    * need execdir instead of exec, typo fix
    
    * better console logging for secrets dir
    
    * typo
    
    * another typo
    
    * another another typo
    
    * Adding logic to avoid sql name collisions
    
    * random id debugging
    
    * Testing code for helm
    
    * Finished debugging sql terraform. Early helm charts
    
    * Trying generic secret helm chart
    
    * All the helm stuff except gitlab
    
    * Missed values reference
    
    * add error handling to helm init
    
    * Rework secrets to handle multiline json
    
    * fixing error handling
    
    * fixing project id env var
    
    * fix second run error
    
    * Break rails connection info into separate helm chart
    
    * Break rails connection info into separate helm chart
    
    * and fix the script
    
    * Finishing new rails-secret chart
    
    * abandon separate rails chart
    
    * abandon separate rails chart
    
    * need to base64 encode secrets
    
    * Start testing the gitlab helm chart
    
    * need more sleep?
    
    * Extending wait time
    
    * even longer sleep
    
    * fixing paths after local troubleshooting
    
    * fixing one more path
    
    * fixing one more path
    
    * try different variable format
    
    * try different variable format
    
    * fixing conflicts
    
    * Everything working. DNS explanation

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ The intention of this project is to allow a user to set up a fully functional gi
 ## Instructions
 Start the process from the home directory of your Cloud Shell by copy and pasting the following command. Everything else should take care of itself:
 
-`curl https://raw.githubusercontent.com/WarpRat/capstone/master/bootstrap.sh | bash -i -`
+```bash
+cd
+curl https://raw.githubusercontent.com/WarpRat/capstone/master/bootstrap.sh > ~/
+chmod +x bootstrap.sh
+./bootstrap.sh
+```
+
+## Limitations
+Although GKE documentation suggests using a wildcard DNS service like xip.io or nip.io to get letsencrypt certificates without having to register a domain name and set up DNS, in practice this is nearly impossible. Letsencrypt limits certificates to 50 per week per domain. Since xip.io and nip.io are popular global services this limit has almost certainly been reached at any given time. In the future this project will be updated to allow the user to supply a domain name that is controlled by GCP's DNS service to make the entire deployment seamless.
--- bin/k8s_bootstrap.sh
+++ bin/k8s_bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# sed function to make things easier
+sed_values() {
+    sed -i "s/$1/$2/g" capstone/helm/values/gitlab_values.yaml
+}
+
 # Set up the tiller RBAC service account and binding outside of configuration management since tiller is
 # needed for helm to do kubernetes configuration management.
 echo "Setting up kubectl to use the new cluster"
@@ -10,25 +15,49 @@ gcloud container clusters get-credentials capstone-project-cluster --region=$(gc
 kubectl create serviceaccount tiller --namespace kube-system && \
   kubectl create clusterrolebinding tiller-admin-binding --clusterrole=cluster-admin --serviceaccount=kube-system:tiller && \
   helm init --service-account=tiller && \
-  echo "Tiller service acocunt installed" || \
+  echo "Tiller service acocunt installed" && \
+  sleep 30 || \
   echo "Tiller appears to have been installed already on this cluster. Moving on."
 
+helm repo add gitlab https://charts.gitlab.io/
 helm repo update
 
-sleep 15
 echo "Checking that helm is properly initialized"
 helm version > /dev/null 2>&1 && echo "Helm appears to have initialized inside your cluster properly." \
                               || echo "Something appears to have gone wrong with helm. Please try again."
 
 cp ~/.capstone_secure/gcs-key.json ~/capstone/helm/charts/secrets/
-helm upgrade gitlab-ns ./capstone/helm/charts/common --install
-helm upgrade gitlab-pg ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/gitlab-pg.yaml --install --set seclit.password=$(cat .capstone_secure/db.pw) --namespace gitlab
-helm upgrade google-application-credentials ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/storage-creds.yaml --install --set fileLit.gcs-application-credentials-file=gcs-key.json --namespace gitlab
-helm upgrade gitlab-rails-storage ./capstone/helm/charts/secrets -f ./capstone/helm/charts/secrets/values/rails.yaml --install --namespace gitlab \
-                                                    --set seclit.google_project=${GOOGLE_CLOUD_PROJECT} \
-                                                    --set seclit.google_client_email=gitlab-storage-sa@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com \
-                                                    --set fileLit.google_json_key_string=gcs-key.json \
-                                                    --set extrakey=connection
-rm ./capstone/helm/charts/secrets/gcs-key.json
+cat > ~/capstone/helm/charts/secrets/rails.yaml <<EOF
+provider: Google
+google_project: ${GOOGLE_CLOUD_PROJECT}
+google_client_email: gitlab-storage-sa@${GOOGLE_CLOUD_PROJECT}.iam.gserviceaccount.com
+google_json_key_strin: $(cat ~/.capstone_secure/gcs-key.json)
+EOF
+
+helm upgrade gitlab-ns ~/capstone/helm/charts/common --install
+helm upgrade gitlab-pg ~/capstone/helm/charts/secrets -f ~/capstone/helm/charts/secrets/values/gitlab-pg.yaml --install --set seclit.password=$(cat ~/.capstone_secure/db.pw) --namespace gitlab
+helm upgrade google-application-credentials ~/capstone/helm/charts/secrets -f ~/capstone/helm/charts/secrets/values/storage-creds.yaml --install --set fileLit.gcs-application-credentials-file=gcs-key.json --namespace gitlab
+helm upgrade gitlab-rails-storage ~/capstone/helm/charts/secrets -f ~/capstone/helm/charts/secrets/values/rails.yaml --install --namespace gitlab \
+                                                    --set fileLit.connection=rails.yaml
+rm ~/capstone/helm/charts/secrets/gcs-key.json ~/capstone/helm/charts/secrets/rails.yaml
 
 # Set environment variables for everything we need to sed into the gitlab values.yaml
+export GITLAB_INGRESS=$(gcloud compute addresses describe gitlab --region us-west1 --format 'value(address)')
+export DB_NAME=$(gcloud compute project-info describe --format 'value(commonInstanceMetadata[db_name])')
+export DB_IP=$(gcloud sql instances describe ${DB_NAME} --format 'value(ipAddresses[0].ipAddress)')
+export REDIS_IP=$(gcloud redis instances describe gitlab --region=us-west1 --format 'value(host)')
+export USER_EMAIL=$(gcloud config get-value account 2> /dev/null)
+sed_values '__DB_IP__' ${DB_IP}
+sed_values '__REDIS_IP__' ${REDIS_IP}
+sed_values '__CERT_MANAGER_EMAIL__' ${USER_EMAIL}
+sed_values '__INGRESS_IP__' ${GITLAB_INGRESS}
+
+helm install --name gitlab -f capstone/helm/values/gitlab_values.yaml --version 1.7.1 --namespace gitlab gitlab/gitlab
+
+export GITLAB_HOSTNAME=$(kubectl get ingresses.extensions gitlab-unicorn -n gitlab -o jsonpath='{.spec.rules[0].host}')
+echo "Your GitLab URL is: https://${GITLAB_HOSTNAME}"
+
+echo "You can log in to your instance and start using it with the username root and the following password:"
+kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -o go-template='{{.data.password}}' | base64 -d && echo
+
+echo "Please save this password somewhere and set up a less privileged user for daily tasks."
