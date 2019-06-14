bb2c141aab1eecc0f7a3e9b16b2ebdd0c505410d
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2019-06-13 22:01:01 -0700

    Everything working. DNS explanation

--- bin/tf_apply.sh
+++ bin/tf_apply.sh
@@ -18,8 +18,36 @@ tf_apply() {
 tf_apply $HOME/capstone/terraform/service-accounts
 tf_apply $HOME/capstone/terraform/storage
 tf_apply $HOME/capstone/terraform/database "-var gitlab_db_pass=$(cat $HOME/.capstone_secure/db.pw)"
+
+sleep 3
+clear
+echo "Do you own a domain name that you'd like to use for this gitlab server?"
+echo "This is not required, however the certificate mananger will be unable to issue a valid certificate otherwise."
+echo "You can still create the cluster and install gitlab, however the Kubernetes gitlab runner will not register properly."
+echo "It is highly recommended that you use a proper domain name."
+read -p "Would you like you enter your domain name now?" -n 1 -r
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo
+  echo "Gitlab will automatically add subdomains for gitlab and registry,"
+  echo "using an unused domain or unused subdomain will allow DNS to be configured with a wildcard."
+  read -p "What is the domain name you'd like to use?" domain_name
+  echo
+  echo "Thanks! Now is a good time to set up DNS records for gitlab.${domain_name} and registry.${domain_name}."
+  echo "Alternatively a wildcard DNS record for *.${domain_name} works as well."
+  sed -i "s/__INGRESS_IP__.xip.io/${domain_name}/" capstone/helm/values/gitlab_values.yaml
+  read -p "Press any key when you're ready to continue. Setting DNS up now will allow it to propagate while the cluster is provisioned." -n 1 -r
+else
+  echo
+  echo "No problem. See the README for an explanation of the limitations of not using a domain name."
+  read -p "Press any key to continue." -n 1 -r
+fi
+
+
 tf_apply $HOME/capstone/terraform/cluster
 
+echo
 echo "Finished installing infrastructure, time to configure the cluster"
 sleep 2
 $HOME/capstone/bin/k8s_bootstrap.sh
