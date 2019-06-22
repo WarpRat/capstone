2f179c7818d565d375ca1ef14d7d197adc9f15d0
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2019-06-22 09:38:11 -0700

    GCP Cloud Shell is fighting over terraform versions

--- bin/tf_apply.sh
+++ bin/tf_apply.sh
@@ -4,12 +4,12 @@ set -eo pipefail
 #TODO: Consider adding an option to supress apply output if private key output is needed to access from remote state
 tf_apply() {
     cd $1
-    terraform init
-    terraform plan -out=planned_apply $2
+    tf11 init
+    tf11 plan -out=planned_apply $2
     read -p "Apply this plan? " -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-      terraform apply planned_apply
+      tf11 apply planned_apply
     else
       echo -e "\nThere is no need to rerun the full bootstrap script. You can resume from this point by running the following script $HOME/capstone/bin/tf_apply.sh when you're ready."
     fi
--- bin/tf_clean.sh
+++ bin/tf_clean.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 # A terraform cleanup script for dev work
-find $HOME/capstone/ -type d -name '.terraform' -execdir terraform destroy -auto-approve \;
+find $HOME/capstone/ -type d -name '.terraform' -execdir tf11 destroy -auto-approve -var gitlab_db_pass=$(cat $HOME/.capstone_secure/db.pw)\;
 echo "Running it again to clean up lingering resources."
-find $HOME/capstone/ -type d -name '.terraform' -execdir terraform destroy -auto-approve \;
+find $HOME/capstone/ -type d -name '.terraform' -execdir tf11 destroy -auto-approve -var gitlab_db_pass=$(cat $HOME/.capstone_secure/db.pw)\;
