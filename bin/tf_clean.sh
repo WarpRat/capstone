84b26cb6760788dfa02e0dcd606d446ef0046219
WarpRat <23439640+WarpRat@users.noreply.github.com>
2019-06-22 10:23:57 -0700

    Fixing cleanup script and adding documentation. (#18)
    * minor fixes to cleanup script
    
    * added cleanup to README

--- bin/tf_clean.sh
+++ bin/tf_clean.sh
@@ -2,7 +2,9 @@
 
 set -euo pipefail
 
+. $HOME/.caprc
+
 # A terraform cleanup script for dev work
-find $HOME/capstone/ -type d -name '.terraform' -execdir tf11 destroy -auto-approve -var gitlab_db_pass=$(cat $HOME/.capstone_secure/db.pw)\;
+find $HOME/capstone/ -type d -name '.terraform' -execdir tf11 destroy -auto-approve -var gitlab_db_pass=$(cat $HOME/.capstone_secure/db.pw) \;
 echo "Running it again to clean up lingering resources."
-find $HOME/capstone/ -type d -name '.terraform' -execdir tf11 destroy -auto-approve -var gitlab_db_pass=$(cat $HOME/.capstone_secure/db.pw)\;
+find $HOME/capstone/ -type d -name '.terraform' -execdir tf11 destroy -auto-approve -var gitlab_db_pass=$(cat $HOME/.capstone_secure/db.pw) \;
